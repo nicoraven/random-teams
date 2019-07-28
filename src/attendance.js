f1e6aee2e3663f4ee7972840fb6054b917a13573
khyreerusydi <nicoraventbh@gmail.com>
2019-07-28 18:46:28 +0800

    display present and absent names with total count for each

--- src/attendance.js
+++ src/attendance.js
@@ -24,21 +24,47 @@ class Attendance extends Component {
 
             for (let i = 0; i < inputQuery.length; i++) {
                 let queryPair = inputQuery[i].split('=');
-                queryParams[queryPair[0]] = queryPair[1].split('+');
+                queryParams[queryPair[0]] = queryPair[1].split('+').sort((a, b) => {
+                    return a.toLowerCase().localeCompare(b.toLowerCase());
+                });
             };
 
-            this.setState({present: queryParams.present, absent: queryParams.absent}, () => {
-                console.log("Loaded!", this.state);
-            });
+            this.setState({present: queryParams.present, absent: queryParams.absent});
         }
     }
 
+    // componentDidUpdate = () => {
+    //     console.log("state updated!");
+    // }
+
     render() {
+        console.log("rendering!", this.state);
+        let sortedPresent = this.state.present;
+        let sortedAbsent = this.state.present;
+
+        let presentCount = this.state.present.length;
+        let absentCount = this.state.absent.length;
+
+        let presentNames = sortedPresent.map((name, index) => {
+            name = name.charAt(0).toUpperCase() + name.slice(1);
+            return (<li key={index} >{name}</li>)
+        });
+        let absentNames = sortedAbsent.map((name, index) => {
+            name = name.charAt(0).toUpperCase() + name.slice(1);
+            return (<li key={index} >{name}</li>)
+        });
+
         return (
             <div>
                 <input type="text" id="name-input" name="name"/>
-                <p>Present: <span id="present-count"></span></p>
-                <ul id="present"></ul>
+                <p>Present: <span id="present-count">{presentCount}</span></p>
+                <ul id="present">
+                    {presentNames}
+                </ul>
+                <p>Absent: <span id="present-count">{absentCount}</span></p>
+                <ul id="absent">
+                    {absentNames}
+                </ul>
             </div>
         )
     }
