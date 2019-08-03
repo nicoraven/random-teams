68170e143c2753a8181d8ee48054b9fee9c13387
khyreerusydi <nicoraventbh@gmail.com>
2019-08-03 16:35:08 +0800

    feat: checkbox toggles between select pax per group or custom groups

--- src/groupings.js
+++ src/groupings.js
@@ -13,12 +13,23 @@ class Groupings extends Component {
         });
     }
 
-    render() {
+    toggleCustom = () => {
+        let custom = !this.state.custom;
+        this.setState({custom: custom});
+    }
 
+    render() {
         return (
             <div id="groupings-wrapper">
                 <h2>Groups</h2>
-                <Sorting custom={this.state.custom} pax={this.state.pax} changeHandler={this.changeHandler} />
+                <div id="grouping-menu">
+                    <Sorting custom={this.state.custom} pax={this.state.pax} changeHandler={this.changeHandler} />
+                    <div>
+                        <label>Custom Groups</label>
+                        <input type="checkbox" onChange={this.toggleCustom} />
+                    </div>
+                </div>
+
                 <div id="grouping-results"></div>
             </div>
         )
@@ -29,19 +40,19 @@ class Sorting extends Component {
 
     render() {
         return this.props.custom ? (
-            <div id="grouping-menu">
-                <input id="custom-group" placeholder="Custom no. of groups" />
-            </div>
+            <React.Fragment>
+                <input className="group-input" id="custom-group" placeholder="Custom no. of groups" />
+            </React.Fragment>
         ) : (
-            <div id="grouping-menu">
-                <select id="pax-group" defaultValue={this.props.pax} onChange={this.props.changeHandler}>
+            <React.Fragment>
+                <select className="group-input" id="pax-group" defaultValue={this.props.pax} onChange={this.props.changeHandler}>
                     <option value="1">Pax per Group</option>
                     <option value="2">Pairs</option>
                     <option value="3">Threes</option>
                     <option value="4">Fours</option>
                     <option value="5">Fives</option>
                 </select>
-            </div>
+            </React.Fragment>
         )
     }
 }
